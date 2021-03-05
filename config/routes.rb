1561415f7843aba44b2034b9cb8bfc35cba71975
Niklas von Heyden <niklas.heyden@me.com>
2021-03-05 10:48:07 +0100

    refactor weeks controller and update pundit policies

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
+  root to: 'weeks#current_week'
 
   resources :user do
     resources :weeks, only: [:index, :show, :create]
